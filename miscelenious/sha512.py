234c136ecb1ebded1a68bc77736d04e296ad8dc0
Anubhob <anubhob493@gmail.com>
2025-03-26 21:11:32 +0530

    new midpay.app prototyped

--- miscelenious/sha512.py
+++ miscelenious/sha512.py
@@ -7,7 +7,7 @@ def compute_sha512(message: str) -> str:
     return hashlib.sha512(message.encode()).hexdigest()
 
 if __name__ == "__main__":
-    text = "Hello, world!"
+    text = "Hello, world"
     sha256_result = compute_sha256(text)
     sha512_result = compute_sha512(text)
 
