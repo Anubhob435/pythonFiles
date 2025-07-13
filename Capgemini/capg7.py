20a7dba5b762213b72022ed73fd5eb824ea3739d
Anubhob <anubhob493@gmail.com>
2025-07-13 13:59:16 +0530

    capg7

--- Capgemini/capg7.py
+++ Capgemini/capg7.py
@@ -1,1 +1,17 @@
-#hello comment 
+l = int(input())
+r = int(input())
+
+range_list = []
+score = 0
+
+points = [3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
+
+for i in range(l, r + 1):
+    range_list.append(i)
+    
+    
+for j in points:
+    if j in range_list:
+        score += 1
+
+print(score)
