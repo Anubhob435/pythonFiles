d3a9200e8b9121dc8a14490dbf8bce14954b54c2
Anubhob <anubhob493@gmail.com>
2025-07-13 13:53:49 +0530

    capg6

--- Capgemini/capg6.py
+++ Capgemini/capg6.py
@@ -1,1 +1,15 @@
-#hello world
+def solve(N, heights):
+    count = 1  # First building can always watch sunset
+    max_height = heights[0]
+    
+    for i in range(1, N):
+        if heights[i] > max_height:
+            count += 1
+            max_height = heights[i]
+    
+    print(count)
+    
+N = 5
+Heights = [9, 11, 10, 8, 13]
+
+solve(N, Heights)
